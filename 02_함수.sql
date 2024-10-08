b260161dcb19495d63e2953c3a6a3539f93abf56
thesky3303 <thesky3303@gmail.com>
2024-10-08 17:21:58 +0900

    1008

---
+++ 02_함수.sql
@@ -0,0 +1,274 @@
+-- 함수 : 컬럼의 값을 읽어서 연산을 한 결과를 반환
+
+-- 단일 행 함수 : N개의 값을 읽어서 연산 후 N개의 결과를 반환
+
+-- 그룹 함수 : N개의 값을 읽어서 연산 후 1개의 결과를 반환 (합계, 평균, 최대, 최소)
+
+-- 함수는 SELECT 문의 
+-- SELECT 절, WHERE 절, ORDER BY 절, GROUP BY 절, HAVING 절 사용 가능
+
+------------------------------- 단일 행 함수 -------------------------------
+
+-- LENGTH(컬럼명 | 문자열) : 길이 반환
+SELECT EMAIL, LENGTH(EMAIL)
+FROM EMPLOYEE;
+
+
+-------------------------------------------------------------
+
+-- INSTR(컬럼명 | 문자열, '찾을 문자열' [, 찾기 시작할 위치 [, 순번]] )
+-- 지정한 위치부터 지정한 순번째로 검색되는 문자의 위치를 반환
+
+-- 문자열을 앞에서부터 검색하여 첫번째 B 위치 조회
+-- AABAACAABBAA
+
+SELECT INSTR('AABAACAABBAA','B') FROM DUAL; -- 3
+
+-- 문자열을 5번째 문자부터 검색하여 첫번째 B 위치 조회
+SELECT INSTR('AABAACAABBAA','B',5) FROM DUAL; -- 9
+
+-- 문자열을 5번째 문자부터 검색하여 두번째 B 위치 조회
+SELECT INSTR('AABAACAABBAA','B',5,2) FROM DUAL; -- 10
+
+-- EMPLOYEE 테이블에서 사원명, 이메일, 이메일 중 '@' 위치 조회
+SELECT EMP_NAME, EMAIL, INSTR(EMAIL, '@') 
+FROM EMPLOYEE;
+
+-----------------------------------------------------
+
+-- SUBSTR('문자열' | 컬럼명, 잘라내기 시작할 위치 [, 잘라낼 길이] )
+-- 컬럼이나 문자열에서 지정한 위치부터 지정된 길이만큼 문자열을 잘라내서 반환
+--> 잘라낼 길이 생략 시 끝까지 잘라냄
+
+-- EMPYLOEE 테이블에서 사원명, 이메일 중 아이디만 조회
+-- sun_di@or.kr -> sun_di
+
+SELECT EMP_NAME,SUBSTR(EMAIL,1, INSTR(EMAIL, '@') - 1 ) 아이디
+FROM EMPLOYEE;
+
+---------------------------------------------------------------
+
+
+-- TRIM( [ [옵션] '문자열' | 컬럼명 FROM ] '문자열' | 컬럼명 )
+-- 주어진 컬럼이나 문자열의 앞, 뒤, 양쪽에 있는 지정된 문자를 제거
+--> 양쪽 공백 제거에 많이 사용함
+
+-- 옵션 : LEADING(앞쪽), TRAILING(뒤쪽), BOTH(양쪽,기본값)
+
+SELECT TRIM('                     H E L L O             ')
+FROM DUAL; -- 양쪽 공백 제거
+
+SELECT TRIM(BOTH '#' FROM '####안녕####')
+FROM DUAL;
+
+--------------------------------------------------------------
+
+-- 숫자 관련 함수
+
+-- ABS(숫자 | 컬럼명) : 절대값
+SELECT ABS(10), ABS(-10) FROM DUAL;
+
+SELECT '절대값 같음' FROM DUAL
+WHERE ABS(10) = ABS(-10); -- WHERE 절 함수 작성 가능
+
+
+-- MOD(숫자 | 컬럼명, 숫자 | 컬럼명) : 나머지값 반환
+
+--EMPLOYEE 테이블에서 사원의 월급을 100만으로 나눴을 때 나머지 조회
+
+SELECT EMP_NAME,SALARY,MOD(SALARY, 1000000)
+FROM EMPLOYEE;
+
+-- EMPLOYEE 테이블에서 사번이 짝수인 사원의 사번, 이름 조회
+SELECT EMP_ID, EMP_NAME
+FROM EMPLOYEE
+WHERE 0 = MOD(EMP_ID, 2);
+
+-- EMPLOYEE 테이블에서 사번이 홀수인 사원의 사번, 이름 조회
+SELECT EMP_ID, EMP_NAME
+FROM EMPLOYEE
+WHERE 0 != MOD(EMP_ID, 2);
+
+
+-- ROUND(숫자 | 컬럼명 [, 소수점 위치]) : 반올림 
+
+SELECT ROUND(123.456) FROM DUAL; -- 123, 소수점 첫 번째 자리에서 반올림
+
+SELECT ROUND(123.456, 1) FROM DUAL; 
+--123.5, 소수점 두번째 자리에서 반올림(소수점 첫번째 자리까지 표기)
+
+SELECT ROUND(123.456,0) FROM DUAL; -- 소수점 첫 번째 자리에서 반올림
+
+-- CEIL(숫자 | 컬럼명) : 올림
+-- FLOOR(숫자 | 컬럼명) : 내림
+--> 둘다 소수점 첫째 자리에서 올림/내림 처리
+
+SELECT CEIL(123.1), FLOOR(123.9)  FROM DUAL;
+
+-- TRUNC(숫자 | 컬럼명 [, 위치]) : 특정 위치 아래를 절삭
+SELECT TRUNC(123.456) FROM DUAL; -- 123, 소수점 아래를 절삭
+SELECT TRUNC(123.456,1) FROM DUAL; -- 123.4, 소수점 첫째자리 아래 절삭
+SELECT TRUNC(123.456,-1) FROM DUAL; -- 120, 10의 자리 아래 절삭
+
+
+------------------------------------------------------------------------------
+
+
+/* 날짜(DATE) 관련 함수 */
+
+-- SYSDATE : 시스템에 현재 시간(년, 월, 일, 시, 분, 초)을 반환
+SELECT SYSDATE FROM DUAL;
+
+-- SYSTIMESTAMP : SYSDATE + MS 단위 추가
+SELECT SYSTIMESTAMP FROM DUAL;
+
+
+-- MONTHS_BETWEEN(날짜, 날짜) : 두 날짜의 개월 수 차이 반환
+SELECT ABS(ROUND(MONTHS_BETWEEN(SYSDATE, '2025-01-23'), 3) ) "수강 기간(개월)"
+FROM DUAL; -- 3.463
+
+-- EMPLOYEE 테이블에서
+-- 사원의 이름, 입사일, 근무한 개월수, 근무 년차 조회
+
+SELECT EMP_NAME, HIRE_DATE, 
+CEIL( MONTHS_BETWEEN(SYSDATE, HIRE_DATE) ) "근무한 개월 수",
+CEIL( MONTHS_BETWEEN(SYSDATE, HIRE_DATE) / 12) || '년차' "근무 년차"
+FROM EMPLOYEE;
+
+/* || : 연결 연산자(문자열 이어쓰기)   */
+
+-- ADD_MONTHS(날짜, 숫자) : 날짜에 숫자만큼의 개월 수를 더함. (음수도 가능)
+SELECT ADD_MONTHS(SYSDATE, 4) FROM DUAL;
+SELECT ADD_MONTHS(SYSDATE, -1) FROM DUAL;
+
+
+-- LAST_DAY(날짜) : 해당 달의 마지막 날짜를 구함
+SELECT LAST_DAY(SYSDATE) FROM DUAL;
+SELECT LAST_DAY('2020-02-01') FROM DUAL;
+
+
+-- EXTRACT : 년, 월, 일 정보를 추출하여 리턴(반환)
+-- EXTRACT(YEAR FROM 날짜)  : 년도만 추출
+-- EXTRACT(MONTH FROM 날짜) : 월만 추출
+-- EXTRACT(DAY FROM 날짜)   : 일만 추출
+
+-- EMPLOYEE 테이블에서
+-- 각 사원의 이름, 입사일 조회(입사년도,월,일 )
+-- 2000년 10월 10일
+
+SELECT EMP_NAME,
+EXTRACT(YEAR FROM HIRE_DATE)  || '년 ' ||
+EXTRACT(MONTH FROM HIRE_DATE) || '월 ' ||
+EXTRACT(DAY FROM HIRE_DATE)   || '일' AS 입사일
+FROM EMPLOYEE;
+
+-----------------------------------------------------------------------------------------
+
+-- 형변환 함수
+-- 문자열(CHAR), 숫자(NUMBER), 날짜(DATE) 끼리 형변환 가능
+
+-- 문자열로 변환
+-- TO_CHAR(날짜, [포맷]) : 날짜형 데이터를 문자형 데이터로 변경
+-- TO_CHAR(숫자, [포맷]) : 숫자형 데이터를 문자형 데이터로 변경
+
+-- 숫자 -> 문자 변환 시 포맷 패턴
+-- 9 : 숫자 한칸을 의미, 여러개 작성 시 오른쪽 정렬
+-- 0 : 숫자 한칸을 의미, 여러개 작성 시 오른쪽 정렬 + 빈칸 0 추가
+-- L : 현재 DB에 설정된 나라의 화폐 기호
+
+SELECT TO_CHAR(1234,'99999') FROM DUAL; -- ' 1234'
+SELECT TO_CHAR(1234,'00000') FROM DUAL; -- '01234'
+SELECT TO_CHAR(1234) FROM DUAL; -- '1234'
+
+SELECT TO_CHAR(1000000, '9,999,999') || '원' FROM DUAL;
+
+SELECT TO_CHAR(1000000, 'L9,999,999') FROM DUAL; -- '\1,000,000'
+
+
+-- 날짜 -> 문자 변환 시 포맷 패턴
+-- YYYY : 년도 / YY : 년도 (짧게)
+-- MM : 월
+-- DD : 일
+-- AM 또는 PM : 오전/오후 표시
+-- HH : 시간 / HH24 : 24시간 표기법
+-- MI : 분 / SS : 초
+-- DAY : 요일(전체) / DY : 요일(요일명만 표시)
+
+-- 2024/10/08 16:20:35 화요일
+SELECT TO_CHAR(SYSDATE, 'YYYY/MM/DD HH24:MI:SS DAY') FROM DUAL;
+
+-- 10/08 (화) 
+SELECT TO_CHAR(SYSDATE, 'MM/DD (DY)') FROM DUAL;
+
+-- 2024년 10월 08일 (화)
+SELECT TO_CHAR(SYSDATE, 'YYYY"년" MM"월" DD"일" (DY)') FROM DUAL;
+--SQL Error [1821] [22008]: ORA-01821: 날짜 형식이 부적합합니다
+--> "" 쌍따옴표를 이용해서 단순한 문자로 인식시키며 해결됨
+
+--------------------------------------------------------------------------
+
+-- 날짜로 변환 TO_DATE
+
+-- TO_DATE(문자형 데이터 , [포맷] ) : 문자형 -> 날짜로 변경
+-- TO_DATE(숫자형 데이터 , [포맷] ) : 숫자형 -> 날짜로 변경
+--> 지정된 포맷으로 날짜를 인식함
+
+SELECT TO_DATE('2024-10-08') FROM DUAL;
+SELECT TO_DATE(20230804) FROM DUAL;
+
+SELECT TO_DATE('241008 163230', 'YYMMDD HH24MISS') FROM DUAL;
+--SQL Error [1861] [22008]: ORA-01861: 리터럴이 형식 문자열과 일치하지 않음
+--> 패턴을 적용해서 작성된 문자열의 각 문자가 어떤 날짜 형식인지 인식 시킴
+
+
+-- Y 패턴 : 현재 세기(21세기 == 20XX년 == 2000년대)
+-- R 패턴 : 1세기 기준으로 절반(50년) 이상인 경우는 이전세기 (1900년대)
+-- 			절반(50년) 미만인 경우 현재 세기(2000년대)
+SELECT TO_DATE('800505', 'YYMMDD') FROM DUAL; -- 2080-05-05 00:00:00.000
+SELECT TO_DATE('800505', 'RRMMDD') FROM DUAL; -- 1980-05-05 00:00:00.000
+SELECT TO_DATE('490505', 'RRMMDD') FROM DUAL; -- 2049-05-05 00:00:00.000
+
+
+-- EMPLOYEE 테이블에서 각 직원이 태어난 생년월일 조회
+-- 사원이름, 생년월일 (1965년 10월 08일)
+SELECT * FROM EMPLOYEE;
+
+-- 621231-1985634
+
+-- 1) 주민번호에서 - 앞글자까지만 추출
+-- 2) 추출한 생년월을 DATE 타입으로 변경 -> RR 패턴을 이용하여 1900년도로 바꿈
+-- 3) TO_CHAR를 이용해서 문자열로 변환 -> 1965년 10월 08일
+
+SELECT EMP_NAME,
+TO_CHAR(TO_DATE(SUBSTR(EMP_NO, 1, INSTR(EMP_NO, '-') - 1 ), 'RRMMDD'),
+'YYYY"년" MM"월" DD"일"') AS 생년월일
+FROM EMPLOYEE;
+
+---------------------------------------------------------------------------------
+
+-- 숫자 형변환
+-- TO_NUMBER(문자데이터, [포맷]) : 문자형 데이터를 숫자 데이터로 변경
+
+SELECT '1,000,000' + 500000 FROM DUAL;
+-- SQL Error [1722] [42000]: ORA-01722: 수치가 부적합합니다
+
+SELECT TO_NUMBER('1,000,000','9,999,999') + 500000 FROM DUAL; 
+-- 1,500,000
+
+
+-- NULL 처리 함수
+
+-- NVL(컬럼명, 컬럼값이 NULL일 때 바꿀 값) : NULL인 컬럼값을 다른값으로 변경
+
+/* NULL 과 산술 연산을 진행하면 결과는 무조건 NULL */
+SELECT EMP_NAME, SALARY, NVL(BONUS, 0), SALARY * NVL(BONUS, 0)
+FROM EMPLOYEE;
+
+
+-- NVL2(컬럼명, 바꿀값1, 바꿀값2)
+-- 해당 컬럼의 값이 있으면 바꿀값1로 변경,
+-- NULL 이면 바꿀값2 로 변경
+
+-- EMPLOYEE 테이블에서 보너스를 받으면 'O', 안받으면 'X'
+SELECT EMP_NAME,NVL2(BONUS, 'O', 'X') "보너스 수령"
+FROM EMPLOYEE;
